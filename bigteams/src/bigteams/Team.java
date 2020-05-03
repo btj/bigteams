3f6808c2aec1a52acd2eb3c5a9fd75ac443b3542
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-05-03 10:38:30 +0200

    Fix typo: 'has' -> 'as'

--- bigteams/src/bigteams/Team.java
+++ bigteams/src/bigteams/Team.java
@@ -6,7 +6,7 @@ import java.util.Set;
 /**
  * Each instance of this class represents a team in a student-team graph.
  * 
- * @invar Each of this team's members has this team has its team.
+ * @invar Each of this team's members has this team as its team.
  *    | getMembers().stream().allMatch(s -> s != null && s.getTeam() == this)
  */
 public class Team {
